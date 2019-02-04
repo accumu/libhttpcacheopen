2c6b0cfa4f803d38b0643b58d3d39723efe04027
Niklas Edmundsson <nikke@acc.umu.se>
2019-02-04 15:41:18 +0100

    Ignore vim .*.swp files

--- cacheopen.c
+++ cacheopen.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2017 Niklas Edmundsson <nikke@acc.umu.se>
+ * Copyright 2006-2019 Niklas Edmundsson <nikke@acc.umu.se>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
